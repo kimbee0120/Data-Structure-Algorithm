d528b09ebcf8b734243eac48feaa861c19180066
Eunbi Kim <kimeunb1822@gmail.com>
2020-06-05 10:53:39 -0400

    Edit

---
+++ Premitives/Check_number_Palindrome2.java
@@ -0,0 +1,44 @@
+/**********************************************************************
+ A palindrome is a sequence that reads the same forwards and backward.
+ logrithm and mask approach 
+ **********************************************************************/
+class Check_number_Palindrome2
+{
+    public boolean isPalindrome(int x)
+    {
+        if(x<0) return false; //negative number cannot be a palindrome 
+        int int_len = (int)Math.floor(Math.log10(x))+1; //length of the integer
+        /**********************************************************
+        log10(x) length of x-1.
+        ex) if x is 1221 log10(1221) = 3.08672
+                means 10^3.08672 = 1221
+                Therefore, floor of 3.08672 is 3.0 and 3.0+1 is 4 
+                which is length of the integer x. 
+        **********************************************************/
+        int mask_val = (int)Math.pow(10,(int_len-1)); //value of mask 
+        /*********************************************************
+        if x = 1221, we need to mask with 1000.
+        1221 mask with 1000 will be 1.221 which allow to get the 
+        first integer of x. 
+
+        10^(int_len-1) will provide mask value 
+        **********************************************************/
+
+        //for loop will run until to reach to the middle integer
+        for(int i=0; i<int_len/2; i++)
+        {
+            int first_num = x/mask_val; //get first place of x
+            int last_num = x%10; //get last place of x
+            
+            //compare first_num and last_num
+            if(first_num != last_num) return false;
+
+            x = x%mask_val; //remove first place of x;
+            x = x/10; //remove last place of x;
+            mask = mask/100 // first and last integer removed. mask value need to remove 2 0's
+        }
+
+        return true;
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Data-Structure-Algorithm
-Cracking Coding Interview
+## <b>Data Structure & Algorithm</b>
+*Contents*
+<li> Resume </li>
+<li> Time and Space Complexity</li>
+<li> Premitives </li>
+<li> Arrays </li>
+<li> Strings </li>
+<li> Linked Lists </li>
+<li> Stacks and Queues </li>
+<li> Trees </li>
+<li> Tries,Segment, Red-Black, etc Trees </li>
+<li> Heaps </li>
+<li> Sorting </li>
+<li> Recursion and Backtracking </li>
+<li> Dynamic Programming </li>
+<li> Greedy Algorithms</li>
+<li> Graphs </li>
+<li> Screening </li>
+<li> Concurrency </li>
+
+*Reference Leetcode and Back to Back SWE<br><b>Back to Back SWE</b> is really helpful with their awesome explanation of coding questions and deep algorithm understanding. I highly recommend*
+
+
